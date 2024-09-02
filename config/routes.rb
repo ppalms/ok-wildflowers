ed0ed02ba34600d4d3ab52111978af739008cb0d
Patrick Palmer <ppalmer@pm.me>
2024-09-02 04:15:43 -0500

    Add org ID to permitted sign-up params

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    registrations: "users/registrations"
+  }
 
   root "pages#home"
 
