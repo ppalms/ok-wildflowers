755438dea7e65db64be0ff940083f6b19df4dd67
Patrick Palmer <ppalmer@pm.me>
2024-09-08 10:41:27 -0500

    Redirect to locations on sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: "users/registrations"
+    registrations: "users/registrations",
+    sessions: 'users/sessions'
   }
 
   root "pages#home"
